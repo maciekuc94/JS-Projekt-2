fcf567139bb9c2a2c7a97d6da435a06c813aaca7
Maciej Kuc <maciekuc94@gmail.com>
2024-04-30 12:19:07 +0200

    przetlumaczenie na ang

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ document.addEventListener("DOMContentLoaded", () => {
     amountInput.setAttribute("step", "0.01");
 
     if (amount < 0.01) {
-      resultDiv.textContent = "Minimalna wartość to 0.01";
+      resultDiv.textContent = "Minimum value is 0.01";
       return;
     }
 
@@ -28,8 +28,8 @@ document.addEventListener("DOMContentLoaded", () => {
         2
       )} ${currencyCode} = ${countedAmount} PLN`;
     } catch (error) {
-      console.error("Wystąpił błąd:", error);
-      resultDiv.textContent = "Wystąpił błąd. Spróbuj ponownie później.";
+      console.error("An error occurred:", error);
+      resultDiv.textContent = "An error occurred. Please try again later.";
     } finally {
       loader.classList.add("hidden");
     }
@@ -41,17 +41,17 @@ document.addEventListener("DOMContentLoaded", () => {
     try {
       const response = await fetch(apiUrl);
       if (!response.ok) {
-        throw new Error("Nie można pobrać danych z API");
+        throw new Error("Failed to fetch data from the API");
       }
       const data = await response.json();
       const rate = data?.rates?.[0]?.mid;
       if (rate) {
         return rate;
       } else {
-        throw new Error("Niepoprawny format danych z API");
+        throw new Error("Incorrect data format from the API");
       }
     } catch (error) {
-      console.error("Wystąpił błąd:", error);
+      console.error("An error occurred:", error);
       throw error;
     }
   }
